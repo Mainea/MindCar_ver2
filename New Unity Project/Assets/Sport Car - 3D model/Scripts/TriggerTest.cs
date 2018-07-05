015970d0c6c999deb78a0ec849c1c36d41b30c44
Mainea <dldl8675@gmail.com>
2018-07-05 12:04:55 +0900

    2

---
+++ New Unity Project/Assets/Sport Car - 3D model/Scripts/TriggerTest.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TriggerTest : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+	void OnTriggerEnter(Collider col)
+	{
+		
+			if (col.gameObject.tag == "fence")
+		{
+			transform.Translate(new Vector3(0, 0, -100));
+		}
+		
+
+	}
+
+	void OnCollisionStay(Collision col)
+	{
+
+			if (col.gameObject.tag == "fence")
+		{
+			transform.Translate(new Vector3(0, 0, -100));
+		}
+
+	}
+
+	void OnCollisionExit(Collision col)
+	{
+		if (col.gameObject.tag == "fence")
+		{
+			transform.Translate(new Vector3(0, 0, -100));
+		}
+	}
+}
