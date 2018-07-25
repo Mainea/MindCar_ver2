8de7d328a43f34c45028b18cafc10b94079a279a
이은민 <eunmin@ieunmin-ui-MacBook-Pro.local>
2018-07-25 21:10:44 +0900

    half walls & scenes

---
+++ New Unity Project/Assets/Scripts/MoveScene.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+ 
+public class MoveScene : MonoBehaviour
+{
+    void Start ()
+    {
+        
+    }
+    
+    void Update ()
+    {
+        
+    }
+    public void QuitGame()
+    {
+         Application.Quit();
+    }
+
+    public void ChangeGameScene_0()
+    {
+        SceneManager.LoadScene("0_StartView");
+    }
+
+    public void ChangeGameScene_1()
+    {
+        SceneManager.LoadScene("1_Mode");
+    }
+
+    public void ChangeGameScene_2_1()
+    {
+        SceneManager.LoadScene("2_1_Individual");
+    }
+
+    public void ChangeGameScene_2_2()
+    {
+        SceneManager.LoadScene("2_1_Competition");
+    }
+
+    public void ChangeGameScene_3_1()
+    {
+        SceneManager.LoadScene("default");
+    }
+
+}
